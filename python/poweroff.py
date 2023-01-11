0b2b323834732d486d11ecd73d9573e64610e06a
brave24221 <brave24221@proton.me>
2023-01-10 23:05:25 -0500

    removing poweroff.py. it's useless to me

--- python/poweroff.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# This is a quick script to shut off my computer 
-# instead of repeatedly using the command-line to 
-# type in the commands for it.
-
-import os
-os.system('shutdown now -h')
-
